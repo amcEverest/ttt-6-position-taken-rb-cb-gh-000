9a6db8994a6fc4b82a4e8fa476f7c69dd2b8ed48
Andres Cortez <andres.corteztm@gmail.com>
2018-10-07 07:05:34 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,8 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+index = board []
 def position_taken(board,index)
   if board[] = " " || "" || nil
     false
   else
     true
 end
-
-position_taken()
